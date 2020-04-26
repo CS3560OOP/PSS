349065a9cb7ea6b15100507ac6f330736cb97922
Grant <honeyhomie@gmail.com>
2020-04-25 18:23:29 -0700

    added task type constants for each subclass of tasks

--- pss/lib/constants.dart
+++ pss/lib/constants.dart
@@ -3,3 +3,22 @@ import 'package:flutter/material.dart';
 const kAntiTaskColor = Colors.redAccent;
 const kTransientTaskColor = Colors.greenAccent;
 const kRecurringTaskColor = Colors.amberAccent;
+
+const recurTaskTypes = [
+  "Class",
+  "Study",
+  "Sleep",
+  "Exercise",
+  "Work",
+  "Meal",
+];
+
+const transTaskTypes = [
+  "Visit",
+  "Shopping",
+  "Appointment",
+];
+
+const antiTaskTypes = [
+  "Cancellation",
+];
